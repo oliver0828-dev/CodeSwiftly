52062b23ca17af71497fcb43161d940845a7cd3a
Oliver Park <oliverco0828@gmail.com>
2023-09-19 19:27:21 +0900

    Initial Commit

---
+++ CodeSwiftly/CodeSwiftlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeSwiftlyApp.swift
+//  CodeSwiftly
+//
+//  Created by Oliver Park on 2023/09/19.
+//
+
+import SwiftUI
+
+@main
+struct CodeSwiftlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
